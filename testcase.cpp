2644650dbc2776f82fee84159c3c0cf54c8a82b1
Yutong-Yuan <2408937165@qq.com>
2023-12-13 18:03:08 +0800

    c++11 threadpool

--- testcase.cpp
+++ testcase.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+//用户自定义函数
 int func1(int a,int b,int c)
 {
     c=a+b+c;
@@ -14,22 +15,25 @@ int main()
 {
     {
         ThreadPool threadPool;
-        threadPool.start(6,PoolMode::ModeCached,12);
+        cout<<"press enter to start threadpool:";
         cin.get();
+        threadPool.start(6,PoolMode::ModeCached,12);
         future<int> res[100];
         for(int i=0;i<100;i++)
         {
             function<int(int,int,int)> func=func1;
             res[i]=threadPool.submitTask(func,10,20,30);
-            this_thread::sleep_for(10ms);
+            this_thread::sleep_for(chrono::milliseconds(10));
         }
         for(int i=0;i<100;i++)
         {
             cout<<i<<" : ";
             cout<<res[i].get()<<endl;
         }
+        cout<<"press enter to stop threadpool:";
         cin.get();
     }
+    cout<<"press enter to exit testcase:";
     cin.get();
     return 0;
 }
